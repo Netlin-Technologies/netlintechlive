aeddeaf32d6dcb667784db0e7996995de8a7cc3e
Steve Scott <stevenscottmta@gmail.com>
2025-08-11 11:58:30 +0200

    Home page metadata fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,25 @@
 import { Home } from '@/components/Home'
+import type { Metadata } from 'next'
+import { t } from '@/lib/locales'
+
+export function generateMetadata(): Metadata {
+  const title = t.metaData.homeTitle
+  const description = t.metaData.homeDesc
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      type: 'website'
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title,
+      description
+    }
+  }
+}
 
 export default function HomePage() {
   return <Home />
