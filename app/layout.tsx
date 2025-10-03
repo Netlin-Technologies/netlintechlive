e88ec5d62916bf0b64adb3302d6255a827149a6f
SteveScott77 <stevenscottmta@gmail.com>
2025-10-03 23:31:32 +0200

    Linkedin script

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import React from 'react'
 import './globals.css'
 import { Montserrat, Sora } from 'next/font/google';
+import Script from 'next/script';
 import { ClientLayout } from '@/components/ClientLayout'
 import RouteProgress from '@/components/RouteProgress'
 import { GoogleAnalytics } from '@next/third-parties/google'
@@ -34,6 +35,31 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <head>
         {/* Removed global canonical + hreflang to avoid duplicates. These are now defined per-page via the metadata API. */}
         <meta name="robots" content="index,follow" />
+        <Script id="linkedin-insight-config" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8732041";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight-loader" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l) {
+                window.lintrk = function(a, b) {
+                  window.lintrk.q.push([a, b]);
+                };
+                window.lintrk.q = [];
+              }
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";
+              b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
       </head>
       <body className={`${montserrat.variable} ${sora.variable}`}>
         <PostHogProvider>
@@ -54,6 +80,13 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
             {children}
           </ClientLayout>
         </PostHogProvider>
+        <noscript
+          suppressHydrationWarning
+          dangerouslySetInnerHTML={{
+            __html:
+              '<img height="1" width="1" style="display:none;" alt="" src="https://px.ads.linkedin.com/collect/?pid=8732041&fmt=gif" />',
+          }}
+        />
       </body>
       <GoogleAnalytics gaId="G-LRJNGV7EWL" />
     </html>
